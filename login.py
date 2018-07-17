e8f395b3dca15ab410925d29452171a9f532e911
Shreeraj Shah <shreeraj@Shreerajs-MacBook-Pro.local>
2018-07-17 16:20:32 +0530

    testing...

---
+++ login.py
@@ -0,0 +1,19 @@
+import boto3,json
+
+client = boto3.client('lambda')
+#client = boto3.client('lambda', verify=False) # in case wants to pass through burp/proxy
+credentials = {
+	'login':'john OR 1=1',
+	'password':'letmein',
+	}
+response = client.invoke(
+	FunctionName="login",
+	Payload= json.dumps(credentials)
+	)
+
+res = response['Payload'].read().decode('utf-8')
+print('------ Response -------')
+print(res)
+print('----')
+print('---Response for debug---')
+print(response)
